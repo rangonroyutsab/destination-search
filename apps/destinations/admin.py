126f4aa05720f72805abb2e9afb0841f0aaa6824
Rangon Roy Utsab <rangonroy.rzs17@gmail.com>
2026-08-14 14:49:05 +0600

    chore: registered Destination model in admin

--- apps/destinations/admin.py
+++ apps/destinations/admin.py
@@ -1,1 +1,18 @@
-# Register your models here.
+from django.contrib.gis import admin
+
+from .models import Destination
+
+
+@admin.register(Destination)
+class DestinationAdmin(admin.GISModelAdmin):
+    list_display = (
+        "city",
+        "country",
+        "population",
+        "location",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = ("city", "country")
+    list_filter = ("country",)
+    ordering = ("country", "city", "population")
